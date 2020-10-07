0dc07afa321d11f3ddd082df150be379596a059a
Alistair-Hudson <70953755+Alistair-Hudson@users.noreply.github.com>
2020-10-07 16:33:51 +0300

    Finished a Breakout skeleton. Will comeback to build upon.

---
+++ Unity2D/block_breaker/Block Breaker/Assets/Scripts/Ball.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    // configuration paramters
+    [SerializeField] Paddle paddle1;
+    [SerializeField] float velocityX = 2f;
+    [SerializeField] float velocityY = 3f;
+    [SerializeField] AudioClip[] ballSounds;
+    [SerializeField] float randFactor = 0.2f;
+
+    // state
+    Vector2 paddle2Ball;
+    bool hasStarted = false;
+
+    // Cached Componets
+    AudioSource myAudioSource;
+    Rigidbody2D myRigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        paddle2Ball = transform.position - paddle1.transform.position;
+        myAudioSource = GetComponent<AudioSource>();
+        myRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!hasStarted)
+        {
+            LockBAll2Paddle();
+            LaunchBallOnMouseClick();
+        }
+    }
+
+    private void LaunchBallOnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            myRigidBody.velocity = new Vector2(velocityX, velocityY);
+            hasStarted = true;
+        }
+    }
+
+    private void LockBAll2Paddle()
+    {
+        Vector2 paddlePos = new Vector2(paddle1.transform.position.x,
+                                        paddle1.transform.position.y);
+        transform.position = paddlePos + paddle2Ball;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Vector2 velocityTweak = new Vector2(UnityEngine.Random.Range(0f, randFactor), 
+                                            UnityEngine.Random.Range(0f, randFactor));
+        if (hasStarted)
+        {
+            AudioClip clip = ballSounds[UnityEngine.Random.Range(0, ballSounds.Length)];
+            myAudioSource.PlayOneShot(clip);
+            myRigidBody.velocity += velocityTweak;
+        }
+    }
+}
---
+++ Unity2D/block_breaker/Block Breaker/Assets/Scripts/Block.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    // Paramters
+    [SerializeField] AudioClip breakSound;
+    [SerializeField] GameObject blockBreakVFX;
+    [SerializeField] float vfxTime = 2f;
+    [SerializeField] int maxHits;
+    [SerializeField] Sprite[] hitSprites;
+
+    //Cached data
+    Level level;
+    GameStatus gStatus;
+
+    // State variables
+    [SerializeField] int timesHit; // Searialized for debugging
+    int spriteIndex = 0;
+
+    private void Start()
+    {
+        maxHits = hitSprites.Length;
+        level = FindObjectOfType<Level>();
+        if ("Breakable" == tag)
+        {
+            level.CountBreakableBlocks();
+        }
+        gStatus = FindObjectOfType<GameStatus>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+         
+        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
+        if ("Breakable" == tag)
+        {
+            --maxHits;
+            if (0 >= maxHits)
+            {
+                BreakVFX();
+                gStatus.AddPoints();
+                level.DecreaseBlocks();
+                Destroy(gameObject);
+            }
+            else
+            {
+                ShowNextHitSprite();
+            }
+        }
+    }
+
+    private void ShowNextHitSprite()
+    {
+        GetComponent<SpriteRenderer>().sprite = hitSprites[++spriteIndex];
+    }
+
+    private void BreakVFX()
+    {
+        GameObject sparkles = Instantiate(blockBreakVFX, transform.position, transform.rotation);
+        Destroy(sparkles, vfxTime);
+    }
+}

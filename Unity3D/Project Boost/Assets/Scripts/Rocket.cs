6d1f09198586d3a15b8efd688ef199fc6a2cafcf
Alistair-Hudson <70953755+Alistair-Hudson@users.noreply.github.com>
2020-11-09 17:22:31 +0200

    Rocket asset made, has basic thruster and rotation

---
+++ Unity3D/Project Boost/Assets/Scripts/Rocket.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    [SerializeField] float thrusterSpeed = 5f;
+    [SerializeField] float rotationalSpeed = 5f;
+
+    Rigidbody rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidbody.AddRelativeForce(Vector3.up);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(-Vector3.forward);// * rotationalSpeed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.forward);// * rotationalSpeed * Time.deltaTime);
+        }
+    }
+}

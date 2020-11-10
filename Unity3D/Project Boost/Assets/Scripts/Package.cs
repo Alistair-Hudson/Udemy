76f2b86435e504048856f646c0ec06912b59ce02
Alistair-Hudson <70953755+Alistair-Hudson@users.noreply.github.com>
2020-11-10 15:53:41 +0200

    Created core mechanical gameplay in a 3D environment.

---
+++ Unity3D/Project Boost/Assets/Scripts/Package.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Package : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up);
+        transform.Rotate(Vector3.left);
+        transform.Rotate(Vector3.forward);
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+    }
+}
--- Unity3D/Project Boost/Assets/Scripts/Rocket.cs
+++ Unity3D/Project Boost/Assets/Scripts/Rocket.cs
@@ -2,18 +2,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {
     [SerializeField] float thrusterSpeed = 5f;
     [SerializeField] float rotationalSpeed = 5f;
 
+    bool hasPackage = false;
+
     Rigidbody rigidbody;
+    AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
     {
-        rigidbody = GetComponent<Rigidbody>();    
+        rigidbody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -24,17 +29,72 @@ public class Rocket : MonoBehaviour
 
     private void ProcessInput()
     {
-        if (Input.GetKey(KeyCode.Space))
-        {
-            rigidbody.AddRelativeForce(Vector3.up);
-        }
+        Thrust();
+        RotateZ();
+        RotateX();
+    }
+
+    private void RotateZ()
+    {
+        rigidbody.freezeRotation = true; //take manual control of rotation
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            transform.Rotate(-Vector3.forward);// * rotationalSpeed * Time.deltaTime);
+            transform.Rotate(-Vector3.forward * rotationalSpeed * Time.deltaTime);
         }
         else if (Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.Rotate(Vector3.forward);// * rotationalSpeed * Time.deltaTime);
+            transform.Rotate(Vector3.forward * rotationalSpeed * Time.deltaTime);
+        }
+        rigidbody.freezeRotation = false; //release manual control of rotation
+    }
+
+    private void RotateX()
+    {
+        rigidbody.freezeRotation = true; //take manual control of rotation
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Rotate(-Vector3.left * rotationalSpeed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Rotate(Vector3.left * rotationalSpeed * Time.deltaTime);
+        }
+        rigidbody.freezeRotation = false; //release manual control of rotation
+
+    }
+
+    private void Thrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidbody.AddRelativeForce(Vector3.up * thrusterSpeed * Time.deltaTime);
+            if (!audioSource.isPlaying)// avoid audio layering
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Stop();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        switch(collision.gameObject.tag)
+        {
+            case "Friendly":
+                if (hasPackage)
+                {
+                    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+                }
+                break;
+            case "Package":
+                hasPackage = true;
+                break;
+            default:
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+                break;
         }
     }
 }

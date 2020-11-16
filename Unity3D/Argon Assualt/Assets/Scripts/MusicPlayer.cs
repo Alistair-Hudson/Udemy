e1aa1e87ed8bd08cf33e1b26f6380a1cc0e014fc
Alistair-Hudson <70953755+Alistair-Hudson@users.noreply.github.com>
2020-11-16 16:22:10 +0200

    Begun Argon Assult section of course Built map with a circuit for camera

---
+++ Unity3D/Argon Assualt/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+        StartCoroutine(LoadScene());  
+    }
+
+    IEnumerator LoadScene()
+    {
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene(1);
+    }
+
+  
+}

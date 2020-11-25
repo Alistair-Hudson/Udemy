e135fe3363e05dadd9d4217e4ae30db301ba59ca
Alistair-Hudson <70953755+Alistair-Hudson@users.noreply.github.com>
2020-11-25 15:29:12 +0200

    finished section on modules, packages and investigating __name__ == "__main__"

---
+++ PythonBootCamp/name and main/one.py
@@ -0,0 +1,12 @@
+#one.py
+
+def func():
+    print("PUNC in ONE.PY")
+
+print("TOP LEVEL IN ONE.PY")
+
+if __name__ == "__main__":
+    #execute stuff
+    print("ONE.PY IS BEING RUN DIRECTLY")
+else:
+    print("ONE.PY HAS BEE IMPORTED")
---
+++ PythonBootCamp/name and main/two.py
@@ -0,0 +1,12 @@
+#two.py
+
+import one
+
+print("TOP LEVEL IN TWO.PY")
+
+one.func()
+
+if __name__ == "__main__":
+    print("TWO.PY IS BEIUNG RUN DIRECTLY")
+else:
+    print("TWO.PY HAS BEEN IMPORTED")

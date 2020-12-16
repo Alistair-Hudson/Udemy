e62834c7b0d4aec06edbf47272be79f4f360ce3e
Alistair-Hudson <70953755+Alistair-Hudson@users.noreply.github.com>
2020-12-16 11:32:33 +0200

    Looked at paring strings and user inputs in Java

---
+++ JavaMasterClass/ParsingString.java
@@ -0,0 +1,18 @@
+public class ParsingString {
+
+    public static void main(String[] args) {
+        
+        String numAsString = "2018";
+
+        int num = Integer.parseInt(numAsString);
+        double d = Double.parseDouble(numAsString);
+
+        numAsString += 1;
+        num += 1;
+
+        System.out.println("numAsString = " + numAsString);
+        System.out.println("num = " + num);
+
+    }
+    
+}
---
+++ JavaMasterClass/UserInput.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+import java.util.Scanner;
+
+public class UserInput {
+    
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+       
+        System.out.println("Enter Name: ");
+        String name = scanner.nextLine();
+        System.out.println("Hello " + name);
+
+        System.out.println("What is your birthyear: ");
+        boolean hasNextInt = scanner.hasNextInt();
+        if (hasNextInt){
+
+            int birthYear = scanner.nextInt(); //Note that negfative numbers can be input
+            scanner.nextLine();//This is so that you can use nextLine() to retrieve strings
+            System.out.println("So you are " + (2020 - birthYear));
+
+        }
+        else{
+            System.err.println("Invaild Year of Birth");
+        }
+
+
+        scanner.close();
+
+    }
+}
